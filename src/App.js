53d901a23aa9ff0d8517632c692e2fffff4ebd9d
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2023-02-07 10:46:38 +0000

    Created InputValue Component and displayed Monthly Outgoings and Monthly Income inputs to user

--- src/App.js
+++ src/App.js
@@ -6,10 +6,54 @@ function Heading() {
   )
 }
 
+function InputValue({ valueType }) {
+  return (
+    <div>
+      <h2>{valueType}</h2>
+      <input placeholder="Input Value"></input>
+      <button>Submit</button>
+    </div>
+  )
+}
+
+function TotalMonthlyIncome() {
+  return (
+    <div>
+      <p></p>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <Heading />
+    <>
+      <Heading />
+      <InputValue valueType="Total Monthly Income"/>
+      <InputValue valueType="Total Monthly Outgoings"/>
+    </>
   );
 }
 
 export default App;
+
+/*
+PSEUDOCODE
+
+  DISPLAYED VALUES
+    Monthly Income: Input Interactable
+
+    Monthly Outgoings: Input Interactable
+
+    Total Monthly Available Funds: Calculated
+
+    Weekly Available Funds: Calculated
+
+    Week 1 Funds: Calculated Interactable
+
+    Week 2 Funds: Calculated Interactable
+
+    Week 3 Funds: Calculated Interactable
+
+    Week 4 Funds: Calculated Interactable
+
+*/
--- src/index.js
+++ src/index.js
@@ -15,25 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-/*
-PSEUDOCODE
-
-  DISPLAYED VALUES
-    Monthly Income: Input Interactable
-
-    Monthly Outgoings: Input Interactable
-
-    Total Monthly Available Funds: Calculated
-
-    Weekly Available Funds: Calculated
-
-    Week 1 Funds: Calculated Interactable
-
-    Week 2 Funds: Calculated Interactable
-
-    Week 3 Funds: Calculated Interactable
-
-    Week 4 Funds: Calculated Interactable
-
-*/
